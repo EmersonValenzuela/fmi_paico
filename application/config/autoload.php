ca8a2da48b0ca7fa4db2a20fbd22a6ca19d2295d
Emerson Valenzuela <52836406+EmersonValenzuela@users.noreply.github.com>
2024-05-13 15:22:39 -0500

    :bug: Fixed bug name model 'Consultas'

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('consultas','insertar','delete');
+$autoload['model'] = array('Consultas','Insertar','Delete');
